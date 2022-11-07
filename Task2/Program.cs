5717d82ea01823f2a266b8f0f774040b55a93a72
«Evgeniya <janeLobanova@mail.ru>
2022-11-07 18:12:26 +0300

    Add new task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите 1 число");
+﻿// Напишите программу, которая на вход принимает 
+// два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите 1 число");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите 2 число");
 int b = Convert.ToInt32(Console.ReadLine());
