533f38ec93eb3065c4859be3074a362b898f1c92
«Evgeniya <janeLobanova@mail.ru>
2022-11-07 18:09:15 +0300

    add new task4

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите 1 число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3 число");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max){
+    max = b;
+    if (c > max ){
+        max = c;
+    }
+}
+Console.WriteLine("Максимальное число : " + max);
