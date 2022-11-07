e3ff802a1849eded89f33cf9ca3c5fb91676c65b
«Evgeniya <janeLobanova@mail.ru>
2022-11-07 17:52:24 +0300

    add new task2

---
+++ Task2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите 1 число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b){
+    Console.WriteLine($"{a} - большее число, {b} - меньшее число");
+}
+else Console.WriteLine($"{b} - большее число, {a} - меньшее число");
