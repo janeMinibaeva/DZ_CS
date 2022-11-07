600666ea47dd9fc2526d9924e9fc8cbccc50313a
«Evgeniya <janeLobanova@mail.ru>
2022-11-08 00:07:06 +0300

    add task_10

---
+++ Task10/Program.cs
@@ -0,0 +1,6 @@
+﻿//Напишите программу, которая принимает на вход 
+//трёхзначное число и на выходе показывает вторую цифру этого числа.
+int number = new Random().Next(100,1000);
+Console.WriteLine(number); 
+int result = (number/10)%10;
+Console.WriteLine(result); 
