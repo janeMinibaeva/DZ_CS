df7e04a0fc49273b9fc872cae7967e88ee0140d8
«Evgeniya <janeLobanova@mail.ru>
2022-11-08 14:03:33 +0300

    add new task13

---
+++ Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая с помощью деления выводит
+//третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+int a = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+if (a/100  == 0){
+    Console.WriteLine("Третьей цифры нет");
+}
+else {
+    while (a > 999) {
+        a = a/10;
+    }
+    int result = a%10;
+    Console.WriteLine(result);
+}
+ 
+ 
+ 
+
+
---
+++ Task15/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход цифру, 
+ //обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day > 0 && day < 8){
+    if (day == 6 || day == 7){
+         Console.WriteLine("Выходной");
+        if (day == 6){
+            Console.WriteLine("Суббота");
+            }
+        if (day == 7){
+             Console.WriteLine("Воскресенье");
+            }
+    }
+    else
+    Console.WriteLine("Будний");
+}
+else Console.WriteLine("Ввод неверный. Введите число от 1 до 7");
